f809443fc6737031d1582635cc0b1f1ee127af0a
MacThings <info@sl-soft.de>
2021-05-19 15:13:40 +0200

    .

---
+++ Sleep-o-Mat/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Sleep-o-Mat
+//
+//  Created by Prof. Dr. Luigi on 19.05.21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Sleep-o-Mat/MainWindow.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Sleep-o-Mat
+//
+//  Created by Prof. Dr. Luigi on 19.05.21.
+//
+
+import Cocoa
+
+class MainWindow: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
