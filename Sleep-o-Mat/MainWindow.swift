97ca31d50b5d1f578e6dceeefea2ea94ae62ef10
MacThings <info@sl-soft.de>
2021-05-20 13:21:15 +0200

    .

--- Sleep-o-Mat/MainWindow.swift
+++ Sleep-o-Mat/MainWindow.swift
@@ -25,7 +25,8 @@ class MainWindow: NSViewController {
     
     @IBOutlet weak var run_button: NSButton!
     @IBOutlet weak var sw_running_dot: NSImageView!
-    @IBOutlet weak var sw_not_running_dot: NSImageView!
+    @IBOutlet weak var dm_running_dot: NSImageView!
+    
     
     
     @IBOutlet weak var install_daemon: NSButton!
@@ -35,9 +36,33 @@ class MainWindow: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.preferredContentSize = NSMakeSize(self.view.frame.size.width, self.view.frame.size.height);
-       
+
         run_check()
 
+        init_check()
+        
+        let alert_check = UserDefaults.standard.bool(forKey: "AlertShown")
+        if alert_check == false {
+            let alert = NSAlert()
+            alert.messageText = NSLocalizedString("Important!", comment: "")
+            alert.informativeText = NSLocalizedString("If you make changes please make sure to press the Start/Stop button (if sleepwatcher is running in daemon mode press Reload) to take the changes affect.", comment: "")
+            alert.alertStyle = .warning
+            alert.icon = NSImage(named: "applied")
+            let Button = NSLocalizedString("I understand", comment: "")
+            alert.addButton(withTitle: Button)
+            alert.runModal()
+            UserDefaults.standard.set(true, forKey: "AlertShown")
+            return
+        }
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+    
+    func init_check() {
         let system_sleep_path = UserDefaults.standard.string(forKey: "system_sleep_path")
         if system_sleep_path != nil{
             let splitter = system_sleep_path!.components(separatedBy: "/") //Gibt den letzten Wert des Arrays aus
@@ -96,47 +121,27 @@ class MainWindow: NSViewController {
         if alert_init != nil{
             UserDefaults.standard.set(false, forKey: "AlertShown")
         }
-        
-        let alert_check = UserDefaults.standard.bool(forKey: "AlertShown")
-        if alert_check == false {
-            let alert = NSAlert()
-            alert.messageText = NSLocalizedString("Important!", comment: "")
-            alert.informativeText = NSLocalizedString("If you make changes please make sure to press the Start/Stop button (or if sleepwatcher is running in daemon mode press Reload) to take the changes affect.", comment: "")
-            alert.alertStyle = .warning
-            alert.icon = NSImage(named: "applied")
-            let Button = NSLocalizedString("I understand", comment: "")
-            alert.addButton(withTitle: Button)
-            alert.runModal()
-            UserDefaults.standard.set(true, forKey: "AlertShown")
-            return
-        }
-    }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
     }
     
     func run_check() {
         syncShellExec(path: scriptPath, args: ["run_check"])
         let sw_running = UserDefaults.standard.bool(forKey: "sleepwatcher_running")
         if sw_running == true{
-            self.sw_running_dot.isHidden = false
-            self.sw_not_running_dot.isHidden = true
+            self.sw_running_dot.image=NSImage(named: "NSStatusAvailable")
             self.run_button.title = NSLocalizedString("Stop", comment: "")
         } else {
-            self.sw_running_dot.isHidden = true
-            self.sw_not_running_dot.isHidden = false
+            self.sw_running_dot.image=NSImage(named: "NSStatusUnavailable")
             self.run_button.title = NSLocalizedString("Run", comment: "")
         }
         
         let dm_running = UserDefaults.standard.bool(forKey: "daemon_running")
         if dm_running == true{
             self.install_daemon.title = NSLocalizedString("Uninstall", comment: "")
+            self.dm_running_dot.image=NSImage(named: "NSStatusAvailable")
             self.run_button.title = NSLocalizedString("Reload", comment: "")
         } else {
             self.install_daemon.title = NSLocalizedString("Install", comment: "")
+            self.dm_running_dot.image=NSImage(named: "NSStatusUnavailable")
         }
     }
     
