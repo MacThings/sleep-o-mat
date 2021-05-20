167d00ebff2cd607bbd1a22732b2bbe3a1bfe547
MacThings <info@sl-soft.de>
2021-05-20 13:41:55 +0200

    Added Sparkle

---
+++ Podfile
@@ -0,0 +1,8 @@
+source 'https://github.com/CocoaPods/Specs.git'
+use_frameworks!
+platform :osx, '10.10'
+
+target 'Sleep-o-Mat' do
+  pod 'Sparkle'
+end
+
---
+++ Pods/Target Support Files/Pods-Sleep-o-Mat/Pods-Sleep-o-Mat-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Sleep_o_Mat : NSObject
+@end
+@implementation PodsDummy_Pods_Sleep_o_Mat
+@end
