f23a7f8cee5e70dc6e736f979db03b49607c9572
MacThings <info@sl-soft.de>
2021-06-08 15:40:50 +0200

    Added own LetsMoveIt

--- Sleep-o-Mat/AppDelegate.swift
+++ Sleep-o-Mat/AppDelegate.swift
@@ -10,14 +10,13 @@ import Cocoa
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    
     func applicationShouldTerminateAfterLastWindowClosed (_
         theApplication: NSApplication) -> Bool {
         return true
     }
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        LetsMoveIt().ToApps()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
